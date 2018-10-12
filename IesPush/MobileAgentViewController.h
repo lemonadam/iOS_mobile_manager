5d11382cf9939636ade0bdbb1a44bad8d6924591
lemon <328686288@qq.com>
2018-10-12 13:02:06 +0800

    up

---
+++ IesPush/MobileAgentViewController.h
@@ -0,0 +1,13 @@
+//
+//  MobileAgentViewController.h
+//  IesPush
+//
+//  Created by wangshuai on 2018/1/23.
+//  Copyright © 2018年 wangshuai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MobileAgentViewController : UIViewController
+
+@end
--- IesPush/MobileAgentViewController.m
+++ IesPush/MobileAgentViewController.m
@@ -1,18 +1,18 @@
 //
-//  MobileNationController.m
+//  MobileAgentViewController.m
 //  IesPush
 //
-//  Created by wangshuai on 17/5/4.
-//  Copyright © 2017年 wangshuai. All rights reserved.
+//  Created by wangshuai on 2018/1/23.
+//  Copyright © 2018年 wangshuai. All rights reserved.
 //
 
-#import "MobileNationController.h"
+#import "MobileAgentViewController.h"
 
-@interface MobileNationController ()
+@interface MobileAgentViewController ()
 
 @end
 
-@implementation MobileNationController
+@implementation MobileAgentViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
